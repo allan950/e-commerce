91521a30200e510b4881cd82ab47348e1fde026a
allan <allan95800yahoo.fr>
2023-02-08 14:41:24 +0100

    Created login form and re-organized template rendering

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,6 +11,6 @@ class HomeController extends AbstractController {
     #[Route('/', name: 'home')]
     public function index(): Response {
 
-        return $this->render('product-list/index.html.twig');
+        return $this->render('index.html.twig');
     }
 }
--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -9,7 +9,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/product', name: 'app_product_list')]
+#[Route('/product')]
 class ProductController extends AbstractController
 {
     #[Route('/list', name: 'app_product_list')]
