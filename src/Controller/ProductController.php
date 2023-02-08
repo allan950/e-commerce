a31902b9063d3e864544f2b50d9af0e81b5deebe
allan <allan95800yahoo.fr>
2023-02-08 12:12:08 +0100

    Changes templates and created dummy data

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,33 +2,36 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/product', name: 'app_product_list')]
 class ProductController extends AbstractController
 {
-    #[Route('/product/list', name: 'app_product_list')]
-    public function index(): Response
+    #[Route('/list', name: 'app_product_list')]
+    public function index(ProductRepository $productRepo): Response
     {
-        $products = "";
+        $products = $productRepo->findAll();
 
         return $this->render('product-list/index.html.twig', [
             'products' => $products,
         ]);
     }
 
-    #[Route('product/list/product/{id<[0-9]+>}', name: 'app_product')]
-    public function retrieveProduct(Request $req): Response {
-
-        $id = $req->attributes->get("id");
-        dd($id);
-
-        $product = "";
+    #[Route('/list/product/{id<[0-9]+>}', name: 'app_product')]
+    public function retrieveProduct(Product $product): Response {
 
         return $this->render('product/index.html.twig', [
             "product" => $product,
         ]);
+    }
+
+    #[Route('/product/add-to-cart', name: 'app_add_product_to_cart')]
+    public function addProductToCart() {
+
     }
 }
